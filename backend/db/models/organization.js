559ce9d054f39d75c42afbf9c1793a28b83945ef
Jon M <jsm22601@gmail.com>
2024-09-24 18:41:40 -0400

    Added org table and migrations as well as adjusted user migrations and seeders

---
+++ backend/db/models/organization.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Organization extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Organization.hasMany(
+        models.User, 
+        {foreignKey: 'orgId', onDelete: 'CASCADE'}
+      )
+    }
+  }
+  Organization.init({
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        len: [1, 255]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Organization',
+  });
+  return Organization;
+};
