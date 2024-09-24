988b6b77e1d09b9b18ee9a4157e8e46f709245d4
Jon M <jsm22601@gmail.com>
2024-09-23 20:08:02 -0400

    added backend login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,37 +1,11 @@
 const router = require('express').Router();
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
 
 router.use(restoreUser);
-// GET /api/restore-user
-router.get(
-    '/restore-user',
-    requireAuth,
-    (req, res) => {
-      return res.json(req.user);
-    }
-);
 
-
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-// GET /api/set-token-cookie
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      firstName: 'Demo',
-      lastName: 'Lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
