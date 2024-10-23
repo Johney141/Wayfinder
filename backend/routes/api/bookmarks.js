754db7f7c42bd1588e40ff525f9e7c8bafe1ad7e
Jon M <jsm22601@gmail.com>
2024-10-23 17:46:55 -0400

    added create and delete backend API routes for bookmarks

--- backend/routes/api/bookmarks.js
+++ backend/routes/api/bookmarks.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/:orgId/current', requireOrg, async (req, res, next) => {
     try {
         const userId = req.user.id;
+
         
 
         const bookmarks = await Bookmarks.findAll({
@@ -22,6 +23,49 @@ router.get('/:orgId/current', requireOrg, async (req, res, next) => {
         res.json({Bookmarks: bookmarks})
     } catch (error) {
         next(error)
+    }
+})
+
+router.post('/:orgId/:articleId', requireOrg, async (req, res, next) => {
+    try {
+        const userId = req.user.id;
+        const articleId = parseInt(req.params.articleId);
+
+        const newBookmark = await Articles.create({
+            userId,
+            articleId
+        })
+
+        const fullBookmark = await Articles.findByPk(newBookmark.id, {
+            include: [
+                {
+                    model: Articles,
+                    attributes: ['id','title']
+                }
+            ]
+        })
+
+        return res.status(201).json(fullBookmark)
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.delete('/:orgId/:bookmarkId', requireOrg, async (req, res, next) => {
+    try {
+        const bookmarkId = parseInt(req.params.bookmarkId);
+        const bookmark = await Bookmarks.findByPk(bookmarkId);
+
+        if(!bookmark) {
+            const err = new Error("Bookmark couldn't be found");
+            err.status = 404;
+            return next(err)
+        }
+        await bookmark.destroy();
+
+        res.json({message: "Successfully deleted", bookmark: bookmark});
+    } catch (error) {
+        next(error);
     }
 })
 
