2eda530e5988280dd400e86bf9790c4b8200205a
Jon M <jsm22601@gmail.com>
2024-12-17 12:50:35 -0500

    finished adding instantserach components

--- frontend/src/components/Utilities/SearchBar/Hits/Hit.jsx
+++ frontend/src/components/Utilities/SearchBar/Hits/Hit.jsx
@@ -2,16 +2,25 @@ import { useNavigate } from 'react-router-dom'
 import './Hit.css'
 import { useSelector } from 'react-redux';
 
-function Hit({ hit }) {
+function Hit({ hit, click }) {
     const navigate = useNavigate();
     const orgId = useSelector(state => state.sessionState.user.Organization.id);
-    console.log(hit)
+
+    const handleItemClick = (event) => {
+        event.preventDefault();  
+    };
+
     
 
     return (
         <div 
             className="hit-item article" 
-            onClick={() => navigate(`/${orgId}/articles/${hit.objectID}`)}
+            onClick={() => {
+                navigate(`/${orgId}/articles/${hit.objectID}`)
+                click();
+                
+            }}
+            onMouseDown={handleItemClick}
         >
             <h3>{hit.title}</h3>
         </div>
--- frontend/src/components/Utilities/SearchBar/SearchBar.jsx
+++ frontend/src/components/Utilities/SearchBar/SearchBar.jsx
@@ -1,9 +1,10 @@
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, Hits, SearchBox, } from 'react-instantsearch';
-// import { IoIosSearch } from "react-icons/io";
 import { useSelector } from 'react-redux';
 import Hit from './Hits/Hit';
 import './SearchBar.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 const client = algoliasearch(
@@ -11,34 +12,55 @@ const client = algoliasearch(
     import.meta.env.VITE_ALGOLIA_SEARCH_API_KEY
 );
 
-// const SearchIcon = () => <IoIosSearch className='search-icon' />;
+
 
 function SearchBar() {
-    const orgId = useSelector(state => state.sessionState.user.Organization.id);
+  const orgId = useSelector(state => state.sessionState.user.Organization.id);
+  const [isFocused, setIsFocused] = useState(false); 
+  const navigate = useNavigate();
+
+
+  const handleSearchSubmit = (event, searchQuery) => {
+    event.preventDefault();  // Prevent default form submission
+    const trimmedSearch = searchQuery.trim();
+    if (trimmedSearch) {
+      navigate(`/${orgId}/articles/search?q=${trimmedSearch}`);
+    }
+  };
+
+  const handleClick = () => {
+    setIsFocused(false);
+  }
 
 
   return (
     <InstantSearch indexName={`org_${orgId}_articles`} searchClient={client}>
       <div className='search-container'>
       <SearchBox 
-          // submitIconComponent={SearchIcon}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
           resetIconComponent={() => <span />}
+          onSubmit={(event) => handleSearchSubmit(event, event.target.querySelector('input').value)}
           classNames={{
             form: 'search-wrapper',
             input: 'search-bar',
             submitIcon: 'search-icon',
             reset: 'is-reset-button'
-          }}  
+          }} 
+          
       />
       </div>
-      <div className='hits-container'>
-        <Hits 
-          hitComponent={Hit}
-          classNames={{
-            list: 'hits-list'
-          }}
-        />
-      </div>
+      {(isFocused ) && (
+        <div className='hits-container'>
+          <Hits 
+            hitComponent={({ hit }) => <Hit hit={hit} click={handleClick}/>}
+            classNames={{
+              list: 'hits-list'
+            }}
+          />
+        </div>
+      )}
+
     </InstantSearch>
   );
 }
