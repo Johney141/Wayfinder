22d5cdd161bbe7188ced0d16aa9f471117b0613f
Jon M <jsm22601@gmail.com>
2024-12-19 18:49:28 -0500

    attempted fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,7 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 
-console.log('Vite Env - ALGOLIA_APP_ID:', process.env.VITE_ALGOLIA_APP_ID);
-console.log('Vite Env - ALGOLIA_SEARCH_API_KEY:', process.env.VITE_ALGOLIA_SEARCH_API_KEY);
+
 
 export default defineConfig(({ mode }) => ({
   plugins: [
